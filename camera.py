b04808033d79b652980bb3c2b333264e207c060a
kakexd <kalle-z@hotmail.com>
2023-05-31 14:11:24 +0300

    commit

--- camera.py
+++ camera.py
@@ -1,25 +1,42 @@
 import cv2
+from tqdm import tqdm
+
+
 # making an index to open every single camer from range 0-9
 # this will search all cameras in this case: one built-in and another is with USB
 # None the less, the index finds 2 cameras and following the code:
 
-for index in range(10):
 
+start_index = 0
+end_index = 9
+
+# Creating a progress bar for identifying camera
+pbar = tqdm(total=start_index - end_index + 1)
+
+cap = None
+
+# Finding any camera available to use
+
+for index in range(start_index, end_index + 1):
     # VideoCapture(index) where there is 2 cameras
     cap = cv2.VideoCapture(index)
 
     if cap.isOpened():
 
         # Finds the camera
-        print(f"camera is opened")
+        print(f"camera {index} is opened")
         break
 
-# There's no camera    
-if not cap.isOpened():
-    exit(-1)
+    # Updating a progressbar
+    pbar.update(1)
 
-# Here we are using the second camera and cap = cv2.VideoCapture(index) points that 0 = built-in and 1 = USB connected
-cap = (cv2.VideoCapture(1) or cv2.VideoCapture(0))
+# Closing the progressbar
+pbar.close()
+
+
+# There's no camera    
+if cap is None or not cap.isOpened():
+    exit(-1)
 
 # Camera opens with grayscale what is colored black and white
 while True:
@@ -28,19 +45,15 @@ while True:
     if not ret:
         break
     
-    #edges = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    #edges = cv2.GaussianBlur(edges, (7, 7), 1.5, 1.5)
-    #edges = cv2.Canny(edges, 0, 30, 3)
-
-    colored = cv2.applyColorMap(frame, cv2.COLORMAP_JET)
-    colored = cv2.GaussianBlur(colored, (7,7), 1.5, 1.5)
-    colored = cv2.Canny(colored, 0, 30, 3)
-    # cv2.imshow('edges', edges)
-    cv2.imshow('colored', colored)
-
-
+    edges = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    edges = cv2.GaussianBlur(edges, (7, 7), 1.5, 1.5)
+    edges = cv2.Canny(edges, 0, 30, 3)
+    colored_edges = cv2.applyColorMap(edges, cv2.COLORMAP_JET)
+    
+    cv2.imshow('edges', edges)
+    cv2.imshow('colored_edges', colored_edges)
     if cv2.waitKey(30) >= 0:
         break
-
+    
 cap.release()
 cv2.destroyAllWindows()
